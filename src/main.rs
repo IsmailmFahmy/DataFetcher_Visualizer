6275f4b26cdd2e5516f658eea15c06a2517d4489
Ismail Fahmy <ismailmn05@gmail.com>
2025-02-26 21:17:24 +0100

    Added feature to retrieve API key from the enviromental variables. Added function to call an http request and return data for a certain stock ticker as json.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::env;
+use reqwest::blocking::Client;
+
 fn main() {
-    println!("Hello, world!");
+
+    let key = fetch_api_key()
+        .unwrap_or_else(|error|format!("No Alpha Vantage API key found, please set the enviromental variable \"AV_Key\" to the API Key\nYou can do this by adding the following to the .bashrc file \nexport AV_Key=*key*\nerror:{:?}",error));
+
+    let response = call_api(key,"IBM")
+        .unwrap_or_else(|e| format!("Error with API request: cause: {}",e ));
+    println!("{response}");
+
+
+
 }
+
+fn fetch_api_key() -> Result<String, env::VarError> {
+    let key = "FH_Key";
+    env::var(key)
+}
+
+
+fn call_api(key: String, ticker: &str) -> Result<String, reqwest::Error> {
+    let ticker = ticker.to_uppercase();
+    let client = Client::new();
+
+    let api_url = format!("https://finnhub.io/api/v1/quote?symbol={ticker}&token={key}");
+
+    let response = client.get(api_url)
+        .send()?
+        .text()?;
+    Ok(response)
+}
+
