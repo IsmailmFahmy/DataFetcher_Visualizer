c1feae2e54e19cb3973b11a45494330fd8c9f525
Ismail Fahmy <ismailmn05@gmail.com>
2025-02-27 19:35:22 +0100

    added function to insert api response to sqlite database

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 use std::env;
 use reqwest::blocking::Client;
-use serde::{Deserialize, Serialize};
-use serde_json;
+
+mod deserialize;
+mod db;
+mod schema;
+
+use deserialize::deserialize;
+use db::insert_to_db;
 
 fn main() {
 
     let key = fetch_api_key()
         .unwrap_or_else(|error|format!("No Alpha Vantage API key found, please set the enviromental variable \"AV_Key\" to the API Key\nYou can do this by adding the following to the .bashrc file \nexport AV_Key=*key*\nerror:{:?}",error));
 
-    let response = call_api(key,"IBM")
+    let ticker: &str = "IBM";
+
+    let response = call_api(key,ticker)
         .unwrap_or_else(|e| format!("Error with API request: cause: {}",e ));
-    println!("{response}");
 
+    let deserialize_data = deserialize(&response, ticker).unwrap();
 
-    let deserialize_data = deserialize(&response).unwrap();
-    println!("{}", deserialize_data.current_price);
+    // println!("{response}");
+    // println!("{:#?}", deserialize_data);
 
+    let x = insert_to_db(deserialize_data);
+    if x.is_ok(){
+        println!("el mafrood teshta8al")
+    }
 }
 
 fn fetch_api_key() -> Result<String, env::VarError> {
@@ -36,32 +47,6 @@ fn call_api(key: String, ticker: &str) -> Result<String, reqwest::Error> {
     Ok(response)
 }
 
-#[derive(Serialize, Deserialize)]
-struct StockData {
-    #[serde(rename = "c")]
-    current_price: f32,
-
-    #[serde(rename = "h")]
-    high: f32,
-
-    #[serde(rename = "l")]
-    low: f32,
-
-    #[serde(rename = "o")]
-    opening : f32,
-
-    #[serde(rename = "pc")]
-    previous_close: f32,
-
-    #[serde(rename = "t")]
-    unix_time: u64,
-}
 
 
-fn deserialize(data : &str)  -> serde_json::Result<StockData> {
-    let stock_data: StockData = serde_json::from_str(data)?;
 
-    Ok(stock_data)
-
-
-}
